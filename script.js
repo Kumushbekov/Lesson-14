6f528d870aebfae1d692eca5e15e8d6cba907039
Kumushbekov <b7030n@gmail.com>
2024-01-31 20:07:04 +0100

    homework

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ let a = prompt("Напишите число 1")
 let b = prompt("Напишите число 2")
 let c = prompt("Напишите число 3")
 
-// ! возможный вариант:
-// ! let a1 = Number(a)
-// ! let b1 = Number (b)
-// ! let c1 = Number (c)
-// ! calculateTriangleArea(a1, b1, c1)
+//  возможный вариант:
+ let a1 = Number(a)
+ let b1 = Number (b)
+ let c1 = Number (c)
+ calculateTriangleArea(a1, b1, c1)
 
-calculateTriangleArea(Number(a), Number(b), Number(c))
+// calculateTriangleArea(Number(a), Number(b), Number(c))
