a9cde4ac0f927eef3c7ec7791ac3c4efc45d266e
Kumushbekov <b7030n@gmail.com>
2024-02-01 22:19:46 +0100

    homework

--- script.js
+++ script.js
@@ -15,20 +15,32 @@
 
 
 
-    function calculateTriangleArea (a, b, c) {
-    let p = (a + b + c) / 2
-    let result = Math.sqrt(p * (p - a) * (p - b) * (p - c))
-    alert("Площадь равна " + result)
+
+//     function calculateTriangleArea (a, b, c) {
+//     let p = (a + b + c) / 2
+//     let result = Math.sqrt(p * (p - a) * (p - b) * (p - c))
+//     alert("Площадь равна " + result)
+// }
+
+// let a = prompt("Напишите число 1")
+// let b = prompt("Напишите число 2")
+// let c = prompt("Напишите число 3")
+
+// ! возможный вариант:
+// ! let a1 = Number(a)
+// ! let b1 = Number (b)
+// ! let c1 = Number (c)
+// ! calculateTriangleArea(a1, b1, c1)
+
+//  calculateTriangleArea(Number(a), Number(b), Number(c))
+
+
+
+function convertCelsius(C) {
+    let result = (9/5)*C+32
+    return result
 }
 
-let a = prompt("Напишите число 1")
-let b = prompt("Напишите число 2")
-let c = prompt("Напишите число 3")
-
-//  возможный вариант:
- let a1 = Number(a)
- let b1 = Number (b)
- let c1 = Number (c)
- calculateTriangleArea(a1, b1, c1)
-
-// calculateTriangleArea(Number(a), Number(b), Number(c))
+let C = prompt("Напишите температуру в градусах Цельсия:")
+let result = convertCelsius(C)
+alert("Температура по Фаренгейту равна " + result)
