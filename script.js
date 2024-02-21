a3dbf51f8b6521c8f2914a7a15d95cc5dc3e1ed1
Kumushbekov <b7030n@gmail.com>
2024-02-21 20:25:49 +0100

    homework

--- script.js
+++ script.js
@@ -405,7 +405,16 @@
 //     }
 // }
 
-function name(firstName, lastName) {
-    let fio = (firstName[0] + ". " + lastName)
-    console.log(fio)
+// function name(firstName, lastName) {
+//     let fio = (firstName[0] + ". " + lastName)
+//     console.log(fio)
+// }
+
+function capitalize(word) {
+    let firstUpperLetter = word[0].toUpperCase()
+    let restOfWord = word.slice(1,word.length).toLowerCase()
+    console.log(firstUpperLetter + restOfWord)
 }
+
+// let word2 = prompt("Введите слово:")
+// capitalize(word2)
