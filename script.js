f904619e86fc652b8aec80d52958bc0d859a23d2
Kumushbekov <b7030n@gmail.com>
2024-03-24 23:52:54 +0100

    homework

--- script.js
+++ script.js
@@ -1061,3 +1061,5 @@
 let image = document.getElementById('image')
 
 image.addEventListener('click', () => {
+     console.log(image.getAttribute('src'))
+    if ('https://cdn-icons-png.flaticon.com/512/4315/4315713.png' === image.getAttribute('src')) {
