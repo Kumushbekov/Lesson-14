3f4940cc6319b3587052947c813aa3709eb7cb2b
Kumushbekov <b7030n@gmail.com>
2024-04-06 23:35:45 +0200

    homework

--- script.js
+++ script.js
@@ -1279,3 +1279,8 @@ if(result.length === 0) {
 } else {
     container.innerHTML = ''
 }        
+
+for(let i = 0; i < result.length; i++) {
+    const userEl = document.createElement('div')
+    const userNameEl = document.createElement('p')
+    const userProfession = document.createElement('p')
